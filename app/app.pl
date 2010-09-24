2b4d7d0b6b2f0ffa331af716ff5700122df4ae22
Alexander Kojevnikov <alexander@kojevnikov.com>
2010-09-24 10:22:33 +1000

    FCGI configuration

--- app/app.pl
+++ app/app.pl
@@ -27,46 +27,57 @@ use strict;
 use warnings;
 
 use Digest::SHA qw (sha256_hex);
+use FCGI;
+use FindBin qw ($Bin);
 use HTML::Entities;
 use LWP::Simple;
 use POSIX;
 use XML::Atom::SimpleFeed;
 
-my $site = 'http://updating.versia.com/';
-# File to keep the last CVS revision.
-my $rev = 'UPDATING.rev';
-# Generated Atom feed.
-my $atom = 'UPDATING.atom';
+my $request = FCGI::Request ();
 
-# Check for a new revision at most once per hour.
-unless (-e $rev and -M $rev < 1/24) {
-    my $url = 'http://www.freebsd.org/cgi/cvsweb.cgi/ports/UPDATING';
-    my $data = get ($url) || die "Could not fetch $url";
-
-    # Get the new revision from HTML.
-    unless ($data =~ /Revision <b>(\d+\.\d+)<\/b>/) {
-        die "Could not parse $url";
-    }
-
-    my $newrev = $1;
-    my $currev = open (REV, "< $rev") ? <REV> : '';
-
-    if ($currev ne $newrev) {
-        open REV, "> $rev";
-        print REV $newrev;
-
-        # Fetch the new UPDATING file.
-        $data = get ("$url?rev=$newrev;content-type=text%2Fplain");
-        open ATOM, "> $atom";
-        print ATOM get_feed ($data);
-    }
+while ($request->Accept () >= 0) {
+    print "Content-type: application/atom+xml\r\n\r\n";
+    print_feed ();
 }
 
-open ATOM, "< $atom" || die "Could not find the feed";
-print <ATOM>;
+sub print_feed {
+    # File to keep the last CVS revision.
+    my $rev = "$Bin/UPDATING.rev";
+    # Generated Atom feed.
+    my $atom = "$Bin/UPDATING.atom";
+
+    # Check for a new revision at most once per hour.
+    unless (-e $rev and -M $rev < 1/24) {
+        my $url = 'http://www.freebsd.org/cgi/cvsweb.cgi/ports/UPDATING';
+        my $data = get ($url) || die "Could not fetch $url";
+
+        # Get the new revision from HTML.
+        unless ($data =~ /Revision <b>(\d+\.\d+)<\/b>/) {
+            die "Could not parse $url";
+        }
+
+        my $newrev = $1;
+        my $currev = open (REV, "< $rev") ? <REV> : '';
+
+        if ($currev ne $newrev) {
+            open REV, "> $rev";
+            print REV $newrev;
+
+            # Fetch the new UPDATING file.
+            $data = get ("$url?rev=$newrev;content-type=text%2Fplain");
+            open ATOM, "> $atom";
+            print ATOM get_feed ($data);
+        }
+    }
+
+    open ATOM, "< $atom" || die "Could not find the feed";
+    print <ATOM>;
+}
 
 sub get_feed {
     my $data = shift;
+    my $site = 'http://updating.versia.com/';
     my $feed = XML::Atom::SimpleFeed->new (
         title   => 'FreeBSD ports/UPDATING',
         link    => $site,
@@ -87,26 +98,27 @@ sub get_feed {
         if ($line =~ /^(\d{8}):/) {
             if (not $trim) {
                 # Add the previous entry.
+                my $updated = substr ($date, 0, 4) . '-' . substr ($date, 4, 2);
+                $updated .= '-' . substr ($date, 6, 2) . 'T00:00:00Z';
                 $feed->add_entry (
                     title   => $title,
                     content => "<pre>$content</pre>",
-                    updated => $date,
+                    updated => $updated,
                     id      => "${site}entry/" . sha256_hex ($date, $title, $content),
                 );
                 # Stop if we have enough entries.
-                if (!$entries--) {
+                if (!--$entries) {
                     last;
                 }
                 $title = $content = '';
             }
             $trim = 0;
-            $date = substr ($1, 0, 4) . '-' . substr ($1, 4, 2);
-            $date .= '-' . substr ($1, 6, 2) . 'T00:00:00Z';
+            $date = $1;
         } elsif ($trim) {
             next;
         } elsif ($line =~ /^\s*(AFFECTS:.*)/) {
             $title = encode_entities ($1);
-            $content = encode_entities ($line);
+            $content = "$date:\n" . encode_entities ($line);
         } else {
             $content .= "\n" . encode_entities ($line);
         }
